14f7c2604fb8823402a1e4761bf67e91f251d3e1
Evance23 <evancebarracks01@gmail.com>
2021-09-09 22:40:53 +0300

    Add error pages

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ class Config:
     General configuration parent class
     '''
     pass
+      MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key=963a9822dbb8bae3f86c8d6fa3792e7b'
 
 
 
