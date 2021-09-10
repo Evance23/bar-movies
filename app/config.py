764b5cf29b413367d19aa9146ccaf5230e1bbccc
Evance23 <evancebarracks01@gmail.com>
2021-09-10 04:04:28 +0300

    Add all changes

--- app/config.py
+++ app/config.py
@@ -1,9 +1,14 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
     pass
-      MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key=963a9822dbb8bae3f86c8d6fa3792e7b'
+         MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key=963a9822dbb8bae3f86c8d6fa3792e7b'
+    
+        MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+        SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 
